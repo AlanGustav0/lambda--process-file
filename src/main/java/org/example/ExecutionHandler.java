d89c33fe93f94d658bf6cf9286312a435d670519
AlanGustav0 <alagustavo946@gmail.com>
2025-08-18 14:46:27 -0300

    feature: Obtendo as informações do bucket

--- src/main/java/org/example/ExecutionHandler.java
+++ src/main/java/org/example/ExecutionHandler.java
@@ -3,12 +3,20 @@ package org.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 
 public class ExecutionHandler implements RequestHandler<S3Event,String> {
 
     @Override
     public String handleRequest(S3Event s3Event, Context context) {
-        System.out.println(s3Event);
+        S3EventNotification.S3EventNotificationRecord record = s3Event.getRecords().get(0);
+
+        String bucketName = record.getS3().getBucket().getName();
+        String objectKey = record.getS3().getObject().getKey();
+
+        System.out.println("Bucket: " + bucketName);
+        System.out.println("Objeto: " + objectKey);
+
         return "Success!";
     }
 }
