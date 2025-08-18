ec3ca5040ad8e82c95dd5cdc552a92400d46d065
AlanGustav0 <alagustavo946@gmail.com>
2025-08-18 15:35:55 -0300

    feature: Implements process file

--- src/main/java/org/example/ExecutionHandler.java
+++ src/main/java/org/example/ExecutionHandler.java
@@ -4,6 +4,15 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ExecutionHandler implements RequestHandler<S3Event,String> {
 
@@ -14,8 +23,25 @@ public class ExecutionHandler implements RequestHandler<S3Event,String> {
         String bucketName = record.getS3().getBucket().getName();
         String objectKey = record.getS3().getObject().getKey();
 
-        System.out.println("Bucket: " + bucketName);
-        System.out.println("Objeto: " + objectKey);
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+
+        try{
+            S3Object s3Object = s3Client.getObject(bucketName,objectKey);
+            S3ObjectInputStream inputStream = s3Object.getObjectContent();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            System.out.println("INICIANDO PROCESSAMENTO DO ARQUIVO");
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("ARQUIVO PROCESSADO COM SUCESSO");
+        } catch (Exception e) {
+            System.out.println("Erro ao processar arquivo" + e);
+        }
 
         return "Success!";
     }
